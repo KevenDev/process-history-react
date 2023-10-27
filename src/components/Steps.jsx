7b0867a31ad877aa82ec4d84aac69f8cefa2d63e
Keven Teles <glevenkeven@gmail.com>
2023-10-27 16:47:00 -0300

    style: estilo do steps

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -314,6 +314,23 @@ const Steps = () => {
                       </div>
                     </div>
                   }
+                  {interesse &&
+                    <div className="flex relative pb-12">
+                      <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                        <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                      </div>
+                      <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#005EB8] inline-flex items-center justify-center text-white relative z-10">
+                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                          <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                          <circle cx="12" cy="7" r="4"></circle>
+                        </svg>
+                      </div>
+                      <div className="flex-grow pl-4">
+                        <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">MANIFESTAÇÃO DE INTERESSE - {horasMovimentacoes(interesse?.movementDatetime)}</h2>
+                        <p className="leading-relaxed">Manifeste seu interesse no patrocínio.</p>
+                      </div>
+                    </div>
+                  }
                   {patrocinioAprovado &&
                     <div className="flex relative">
                       <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#005EB8] inline-flex items-center justify-center text-white relative z-10">
@@ -323,8 +340,8 @@ const Steps = () => {
                         </svg>
                       </div>
                       <div className="flex-grow pl-4">
-                        <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">PATROCÍNIO APROVADO</h2>
-                        <p className="leading-relaxed">Pitchfork ugh tattooed scenester echo park gastropub whatever cold-pressed retro.</p>
+                        <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">PATROCÍNIO APROVADO - - {horasMovimentacoes(patrocinioAprovado?.movementDatetime)}</h2>
+                        <p className="leading-relaxed">Seu patrocínio foi aprovado.</p>
                       </div>
                     </div>
                   }
